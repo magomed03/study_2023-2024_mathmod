80c4fc9137769617b0de595180fcf6159d6bdeea
Magomed Mazhitov <magomed_mazhitov_03@mail.ru>
2024-03-09 17:56:25 +0300

    Added lab05

---
+++ labs/lab05/lab05_1.jl
@@ -0,0 +1,39 @@
+using Plots
+using DifferentialEquations
+
+x0 = 8
+y0 = 11
+
+a = 0.25
+b = 0.025
+c = 0.45
+d = 0.045
+
+
+function ode_fn(du, u, p, t)
+    x, y = u
+    du[1] = -a*u[1] + b * u[1] * u[2]
+    du[2] = c * u[2] - d * u[1] * u[2]
+end
+
+v0 = [x0, y0]
+tspan = (0.0, 60.0)
+prob = ODEProblem(ode_fn, v0, tspan)
+sol = solve(prob, dtmax=0.05)
+X = [u[1] for u in sol.u]
+Y = [u[2] for u in sol.u]
+T = [t for t in sol.t]
+
+plt = plot(dpi=300,legend=false)
+
+plot!(plt, X, Y, color=:blue)
+
+savefig(plt, "lab05_1.png")
+
+plt2 = plot( dpi=300, legend=true)
+
+plot!(plt2, T, X, label="Численность жертв", color=:red)
+
+plot!(plt2, T, Y, label="Численность хищников", color=:green)
+
+savefig(plt2, "lab05_2.png")
